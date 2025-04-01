af78c542c5b883ca29696fdad8aea363c3c05a66
Rob Sansom <rob@example.com>
2025-04-01 02:44:46 +0100

    fix: Remove 'sites' from CTA animation word list

--- js/components/cta-text-animation.js
+++ js/components/cta-text-animation.js
@@ -14,7 +14,6 @@ class CTATextAnimation extends HTMLElement {
             'prototype',
             'apps',
             'flows',
-            'sites',
             'tools',
             'code',
             'style',
